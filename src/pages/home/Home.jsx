884b133889171f5b58478e7545da8529d4a3d834
SemabAli880 <semababid882@outlook.com>
2022-03-12 13:22:14 +0500

    pages completed

---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,12 @@
+import './Home.scss'
+
+const Home = () => {
+  return (
+    <div className='home'>
+        <h1 className="title">Sabi Admin</h1>
+        
+    </div>
+  )
+}
+
+export default Home
